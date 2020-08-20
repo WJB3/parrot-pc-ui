25b2107705fc864b48604a8c5fa216788b790310
wjb <wjb@wjbdeMacBook-Pro.local>
2020-08-20 23:22:43 +0800

    tabs

--- packages/pages/AvatarPage.js
+++ packages/pages/AvatarPage.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import Avatar from '@packages/core/Avatar';
-import Space from '@packages/core/Space'; 
-import Paper from '@packages/core/Paper'; 
-import Image1 from '@packages/assets/images/1.jpg';
-import Tabs from '@packages/core/Tabs';
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
+import 'antd/dist/antd.css';
 
 
 const Page= React.forwardRef((props,ref)=>{
     return <React.Fragment>
-        <Paper square >
-        <Tabs >
-            <div  tab="Tab 1" key="1">1</div>
-            <div  tab="Tab 2" key="2">2</div>
-            <div  tab="Tab 3" key="3">3</div>
-        </Tabs>
-        </Paper>
+        <Tabs defaultActiveKey="1"  >
+    <TabPane tab="Tab 1" key="1" forceRender={false}>
+      Content of Tab Pane 1
+    </TabPane>
+    <TabPane tab="Tab 2" key="2" forceRender={false}>
+      Content of Tab Pane 2
+    </TabPane>
+    <TabPane tab="Tab 3" key="3" forceRender={false}>
+      Content of Tab Pane 3
+    </TabPane>
+  </Tabs>
     </React.Fragment>
 });
 
