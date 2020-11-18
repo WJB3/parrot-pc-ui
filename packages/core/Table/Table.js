a4f766c40cc379e4f65d070df7eaa76c1dc882f8
wjb <wjb@wjbdeMacBook-Pro.local>
2020-11-18 22:38:50 +0800

    table

--- packages/core/Table/Table.js
+++ packages/core/Table/Table.js
@@ -1,1 +1,58 @@
-import
+import React,{useContext} from 'react';
+import {
+    ConfigContext
+} from '@packages/core/ConfigProvider';
+import classNames from '@packages/utils/classNames';
+import TableContext from './context/TableContext';
+import BodyContext from './context/BodyContext';
+import ResizeContext from './context/ResizeContext';
+import "./index.scss";
+
+const MemoTableContent=React.memo(
+    ({children})=>children
+)
+
+const Table=React.forwardRef((props,ref)=>{
+
+    const {
+        prefixCls:customizePrefixCls,
+        className,
+        dataSource,
+        columns
+    }=props;
+
+    const prefixCls=useContext(ConfigContext)?.getPrefixCls("Table",customizePrefixCls);
+
+    let groupTableNode;
+
+    let fullTable=(
+        <div className={classNames(
+            prefixCls,
+            className
+        )}>
+            <MemoTableContent>
+                <div className={`${prefixCls}-Container`}>
+
+                </div>
+            </MemoTableContent>
+        </div>
+    )
+
+    return <div 
+        className={
+            classNames(
+                `${prefixCls}-Wrapper`
+            )
+        }
+    >
+        <TableContext.Provider>
+            <BodyContext.Provider>
+                <ResizeContext.Provider>
+                    {fullTable}
+                </ResizeContext.Provider>
+            </BodyContext.Provider>
+        </TableContext.Provider>
+    </div>
+});
+
+export default Table;
--- packages/pages/common/PaginationPage.js
+++ packages/pages/common/PaginationPage.js
@@ -1,12 +1,22 @@
 
 
-import React from 'react'; 
+import React, { useState } from 'react'; 
 import 'antd/dist/antd.css'; 
 import Pagination from '@packages/core/Pagination';
 import PaginationM from '@material-ui/lab/Pagination';
 import { Pagination as PaginationA } from 'antd';
+
+//返回true 表示复用上一次渲染的内容
+const MemoContext=React.memo(
+    ({children})=>children,
+    (prev,next)=>true
+)
  
 const Page=(props)=>{
+
+    const [count,setCount]=useState(0);
+
+
     return (
         <div style={{padding:200}}>  
             <PaginationA total={60} showLessItems/>
@@ -19,6 +29,12 @@ const Page=(props)=>{
                 showTotal={(total, range) => `${range[0]}-${range[1]} of ${total} items`}
                 showQuickJumper
             />   
+
+            <button onClick={()=>setCount(count+1)}>click me!</button>
+
+            <MemoContext>
+                <div>{count}</div>
+            </MemoContext>
 
         </div>
     )
