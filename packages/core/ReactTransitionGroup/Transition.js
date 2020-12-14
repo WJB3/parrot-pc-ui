b8a13e6b35a0044523fda09054a85724013d68d1
WJB3 <2459666474@qq.com>
2020-12-14 17:27:25 +0800

    parrot-ui

--- packages/core/ReactTransitionGroup/Transition.js
+++ packages/core/ReactTransitionGroup/Transition.js
@@ -1,10 +1,13 @@
 
 
-import React,{useState,useMemo,useRef,useEffect,useCallback,useContext} from 'react';
+import React,{createElement,useMemo,useRef,useEffect,useCallback,useContext} from 'react';
 import TransitionGroupContext from './TransitionGroupContext';
 import useInit from '@packages/hooks/useInit';
 import usePrevState from '@packages/hooks/usePrevState';
 import setRef from '@packages/utils/setRef';
+import ReactDOM from 'react-dom';
+import useStateCallback from '@packages/hooks/useStateCallback';
+import useForkRef from '@packages/hooks/useForkRef';
 
 export const UNMOUNTED="unmounted";
 export const EXITED="exited";
@@ -13,6 +16,7 @@ export const ENTERED="entered";
 export const EXITING="exiting";
 
 function noop() {}
+ 
 
 //1.当visible是false时组件正常是处于exited状态
 //2.当visible 由false变成true时 变成entering->entered
@@ -24,98 +28,24 @@ const Transition=React.forwardRef((props,ref)=>{
         timeout,
         onEnter=noop,
         ...childProps
-    }=props;
+    }=props; 
 
-    const isInit=useInit();
-
-    let nodeRef=useRef(null);
-
-    const { initialStatus }=useMemo(()=>{
-
-        let initialStatus;
-
+    let nodeRef=useRef(null); 
+   
+    const { initialStatus }=useMemo(()=>{ 
+        let initialStatus; 
         if(visibleProp){
             initialStatus=ENTERED
         }else{
             //1.当visible是false时组件正常是处于exited状态
             initialStatus=EXITED
-        }
-
+        } 
         return {
             initialStatus
         }
+    },[]);  
 
-    },[]);
-
-    const getTimeouts=useCallback(()=>{
-        let exit, enter, appear;
-        exit = enter = appear = timeout;
-
-        if (timeout != null && typeof timeout !== 'number') {
-            exit = timeout.exit
-            enter = timeout.enter
-            // TODO: remove fallback for next major
-            appear = timeout.appear !== undefined ? timeout.appear : enter
-        }
-        return { exit, enter, appear };
-    },[timeout]);
-
-    const handleRef=useCallback((node)=>{
-        setRef(nodeRef,node);
-    },[])
-
-    const nextCallback=useRef(null);
-
-    const appearStatus=useRef(null);
-
-    const [ status,setStatus ]=useState(initialStatus);
-
-    const prevStatus=usePrevState(status);
-
-    useEffect(()=>{ 
-        updateStatus(true,appearStatus.current);
-        ()=>{
-            cancelNextCallback();
-        }
-    },[]);
-
-    useEffect(()=>{
-        if(isInit){
-            let nextStatus = null;
-            if(visibleProp){
-                if (status !== ENTERING && status !== ENTERED) {
-                    nextStatus = ENTERING
-                }
-            }
-            updateStatus(false, nextStatus)
-        }
-    },[visibleProp,isInit]);
-
-    const cancelNextCallback=()=>{
-        if(nextCallback.current!==null){
-            nextCallback.current.cancel();
-            nextCallback.current=null;
-        }
-    }
-
-    const updateStatus=(mounting=false,nextStatus)=>{
-        if(nextStatus=null){
-            // nextStatus will always be ENTERING or EXITING.
-            cancelNextCallback()
-            if (nextStatus === ENTERING) {
-                performEnter(mounting)
-            }
-        }else{
-
-        }
-    }
-
-    const performEnter=(mounting)=>{
-        const appearing=mounting;
-
-        onEnter?.(maybeNode, appearing);
-    }
-    
+    const [ status,setStatus ]=useStateCallback(initialStatus); 
 
     //如果状态是unmounted，直接返回空
     if (status === UNMOUNTED) {
@@ -128,17 +58,24 @@ const Transition=React.forwardRef((props,ref)=>{
             Component=children(status,childProps);
         }else{
             Component=React.cloneElement(React.Children.only(children),childProps);
-        }
+        } 
         return {
             TransitionComponent:Component
         }
     },[status,childProps,children]);
 
+    const handleRef=useForkRef(ref,nodeRef);
+  
     return (
         <TransitionGroupContext.Provider value={null}>
-            <TransitionComponent 
-                ref={handleRef}
-            />
+            {
+                React.cloneElement(
+                    TransitionComponent,
+                    {
+                        ref:handleRef
+                    }
+                )
+            }
         </TransitionGroupContext.Provider>
     )
 });
--- packages/pages/Demo.js
+++ packages/pages/Demo.js
@@ -1,80 +1,15 @@
-import React, { useState, useCallback } from 'react';
-import { Transition } from 'react-transition-group';
-import TransitionR from '@packages/core/ReactTransitionGroup/Transition';
-
-class Demo extends React.Component{
-  constructor(props){
-    super(props);
-  }
-
-  static getDerivedStateFromProps(nextProps, prevState){
-    console.log("getDerivedStateFromProps")
-    console.log(prevState)
-    console.log(nextProps)
-    return null;
-  }
-  componentDidUpdate(preProps,preState){
-    console.log("componentDidUpdate")
-    console.log(preProps)
-    console.log(preState)
-  }
-
-  render(){
-    return <div>{this.props.a}</div>
-  }
-  
-}
+import React, { useState, useCallback,useMemo } from 'react'; 
+import TransitionR from '@packages/core/ReactTransitionGroup/Transition'; 
 
 export default function App() {
-  const [visible, setVisible] = useState(false);
-  const [count2, setCount2] = useState(0);
-
-  const duration = 300;
-
-  const defaultStyle = {
-    transition: `opacity ${duration}ms ease-in-out`,
- 
-  }
-
-  const transitionStyles = {
-    entering: { opacity: 1 },
-    entered: { opacity: 1 },
-    exiting: { opacity: 0 },
-    exited: { opacity: 0 },
-  };
-
-
-
+    
   return (
-    <div>
-      <Transition in={visible}  >
-        {state => { 
-          console.log(state)
-          return (
-          <div style={{
-            ...defaultStyle,
-            ...transitionStyles[state]
-          }}>
-            I'm a fade Transition!
-          </div>
-        )}}
-      </Transition>
-      <TransitionR in={visible}  >
-        {state => { 
-          console.log(state)
-          return (
-          <div style={{
-            ...defaultStyle,
-            ...transitionStyles[state]
-          }}>
-            I'm a fade Transition!
-          </div>
-        )}}
-      </TransitionR>
-      <Demo a={count2}/>
-
-      <button onClick={()=>setVisible(!visible)}>aaaaaa</button>
-      <button onClick={()=>setCount2(count2+1)}>aaaaaa</button>
+    <div> 
+      <TransitionR>
+        {(status,childProps)=>{ 
+          return <div>{"bbb"}</div>
+        }}
+      </TransitionR> 
     </div>
   );
 }
